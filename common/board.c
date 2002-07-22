d26c86c5628356ed0a3dd7615f4cb4bf51756f1b
mariusgroeger <mariusgroeger>
2002-07-22 10:30:25 +0000

    Fix copyrights

--- common/board.c
+++ common/board.c
@@ -1,5 +1,8 @@
 /*
  * (C) Copyright 2002
+ * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+ * 
+ * (C) Copyright 2002
  * Sysgo Real-Time Solutions, GmbH <www.elinos.com>
  * Marius Groeger <mgroeger@sysgo.de>
  *
--- common/dlmalloc.c
+++ common/dlmalloc.c
@@ -2,13 +2,6 @@
 
 * Legal stuff
 
-  (C) Copyright 2002
-  Sysgo Real-Time Solutions, GmbH <www.elinos.com>
-  Marius Groeger <mgroeger@sysgo.de>
-  
-  See file CREDITS for list of people who contributed to this
-  project.
-  
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
   published by the Free Software Foundation; either version 2 of
